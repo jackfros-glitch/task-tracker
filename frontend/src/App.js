3a989591bc6416ca762a279a78b2a3a41f0a595e
jackfros <hirekaanm3@gmail.com>
2022-09-26 17:40:09 +0100

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import RouterPage from './routerPage'
+
+
+function App() {
+  return (
+    <div>
+      <RouterPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/routerPage.js
@@ -0,0 +1,13 @@
+import { Route , Routes} from 'react-router-dom'
+
+const RouterPage = () => {
+  return (
+    <>
+    <Routes>
+        
+    </Routes>
+    </>
+  )
+}
+
+export default RouterPage
